14bc3e48e32c3133b703bba1800b96731d4733c1
wataru-hi <146914543+wataru-hi@users.noreply.github.com>
2025-02-04 11:05:26 +0900

    02_01完了

--- main.cpp
+++ main.cpp
@@ -1,53 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <functional>
-#include <Windows.h>
-#include <time.h>
+#include <iostream>
+using namespace std;
 
-void setTimeout(int second) {
+class Enemy {
+public:
+	void Update();
 
-	//ロールバック関数の呼び出し
-	Sleep(second * 1000);
+	void Approach(); // 接近
+	void Attack();   // 攻撃
+	void Retreat();  // 離脱
 
+	// 関数ポインタテーブル
+	static void (Enemy::* table[])();
+
+private:
+	int index = 0;
+};
+
+void Enemy::Approach() {
+	cout << "敵が接近！" << endl;
 }
 
-auto Funciton = [=](int answer) {
-	//ランダム生成
-	int diceNumber = rand();
+void Enemy::Attack() {
+	cout << "敵が攻撃！" << endl;
+}
 
-	//答え合わせ
-	if (answer <= 1) {
-		if (answer == diceNumber % 2) {
-			return printf("答えは%d,正解\n", diceNumber % 6);
-		}
-		else if (answer != diceNumber % 2) {
-			return printf("答えは%d,不正解\n", diceNumber % 6);
-		}
+void Enemy::Retreat() {
+	cout << "敵が離脱" << endl;
+}
+
+void Enemy::Update() {
+
+	// 関数テーブルから関数を実行
+	(this->*table[index])();
+
+	cout << "次の状態に移行 (0: はい、 他: いいえ)";
+	int input;
+	cin >> input;
+
+	if (input == 0) {
+		index = (index + 1) % 3;
 	}
-	else {
-		return printf("指定の数字以外が入力されました");
-	}
+}
 
+// メンバ関数ポインタテーブル
+void (Enemy::* Enemy::table[])() = {
+	&Enemy::Approach, // インデックス0
+	&Enemy::Attack,   // インデックス1
+	&Enemy::Retreat   // インデックス2
+};
 
-	return printf("end");
-	};
+int main() {
 
-int main(int argc, const char* argv[]) {
+	Enemy enemy;
 
-	unsigned int randTime = (unsigned int)time(nullptr);
-	srand(randTime);
-
-	int answer;
-
-	//入力処理
-	printf("半(1)か丁(0)か\n");
-	scanf_s("%d", &answer);
-
-	printf("start\n");
-
-	setTimeout(5);
-
-	Funciton(answer);
+	while (1)enemy.Update();
 
 	return 0;
 }
